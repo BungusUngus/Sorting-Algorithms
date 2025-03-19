ec0213dea0b9b040a4f4b0f29f7710bf09c30338
BungusUngus <xavier.katzakis@hotmail.com>
2025-03-19 16:59:29 +1100

    Algorithims
    Algor

--- Assets/Scripts/LinkedList/MyLinkedList/MyLinkedList.cs
+++ Assets/Scripts/LinkedList/MyLinkedList/MyLinkedList.cs
@@ -9,6 +9,7 @@ namespace OurLinkedList
         public int age = 0;
 
         public Node next;
+        public Node prev;
 
         public Node(string name, int age)
         {
@@ -36,9 +37,11 @@ namespace OurLinkedList
             linkedList.Next();
             linkedList.PrintCurrent();
             linkedList.Restart();
+            linkedList.Next();
             linkedList.DeleteNext();
             linkedList.Next();
             linkedList.PrintCurrent();
+
 
 
             linkedList.PrintAll();
@@ -50,12 +53,15 @@ namespace OurLinkedList
         private Node current;
 
         private Node header;
+        private Node tail;
 
         public LinkedList(Node node)
         {
             header = node;
+            tail = node;
             current = node;
             header.next = null;
+            header.prev = null;
         }
 
         public void InsertNext(Node newNode)
@@ -63,23 +69,98 @@ namespace OurLinkedList
             if (current.next == null)
             {
                 current.next = newNode;
+                newNode.prev = current;
                 newNode.next = null;
+                tail = newNode;
             }
             else
             {
                 newNode.next = current.next;
                 current.next = newNode;
+
+                newNode.next.prev = newNode;
+                newNode.prev = current;
             }
-            current = current.next;
+            current = newNode;
+        }
+
+
+        public void InsertPrev(Node newNode)
+        {
+            if (current.prev == null)
+            {
+                header = newNode;
+                current.prev = newNode;
+                newNode.next = current;
+                newNode.prev = null;
+            }
+            else
+            {
+                newNode.prev = current.prev;
+                current.prev = newNode;
+                newNode.prev.next = newNode;
+                newNode.next = current;
+            }
+            current = newNode;
         }
 
         public bool DeleteNext()
-        {
+        { 
             if (current.next == null) return false;
-            //Node delNode = current.next;
+
+            Node delNode = current.next;
             current.next = current.next.next;
+
+            if(current.next == null)
+            {
+                tail = current;
+            }
+            else
+            {
+                current.next.prev = current;
+            }
+
+            //if c++ you must free the memory
             //delNode = null;
             return true;
+        }
+
+        public bool DeletePrev()
+        {
+            if (current.prev == null) return false;
+
+            //Node delNode = current.next;
+            current.prev = current.prev.prev;
+
+            if (current.prev == null)
+            {
+                header = current;
+            }
+            else
+            {
+                current.prev.next = current;
+            }
+            //if c++ you must free the memory
+            //delNode = null;
+            return true;
+        }
+
+        public void deleteCurrent()
+        {
+            if (Prev())
+            {
+                DeleteNext();
+            }
+            else if(Next())
+            {
+                DeletePrev();
+            }
+            else
+            {
+                header = null;
+                tail = null;
+                current = null;
+            }
         }
 
         public bool Next()
@@ -87,6 +168,16 @@ namespace OurLinkedList
             if (current.next != null)
             {
                 current = current.next;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Prev()
+        {
+            if (current.prev != null)
+            {
+                current = current.prev;
                 return true;
             }
             return false;
@@ -106,7 +197,7 @@ namespace OurLinkedList
 
             do
             {
-                Debug.Log(currentPrint);
+                Debug.Log(currentPrint.name);
                 currentPrint = currentPrint.next;
             } while (currentPrint != null);
         }
